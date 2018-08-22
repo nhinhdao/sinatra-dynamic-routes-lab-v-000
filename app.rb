edea353f3de5af7dab9fd53aa83aee0a2ea09861
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-22 23:34:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
       # params[:number].to_i.times {|i| puts params[:phrase].to_s}
       params[:number].to_i.times {|i| @x << params[:phrase].to_s}
       @x.each { |x|  puts x}
-      # binding.pry
+      binding.pry
    end
 
    get '/say/:word1/:word2/:word3/:word4/:word5' do
