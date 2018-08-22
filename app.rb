0b8b8c55a4efd4da71581d8630c9d250c7ef84d2
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-22 23:16:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
    end
 
    get '/say/:number/:phrase' do
-      params[:number].to_i.times {|i| puts params[:phrase]}
+      params[:number].to_i.times {|i| puts params[:phrase].to_s}
       binding.pry
    end
 
